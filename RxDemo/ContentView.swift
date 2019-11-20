724bf990e7d414f88f76365770c5800d60a856bc
Nursultan Askarbekuly <n.askarbekuly@akvelon.com>
2019-11-20 18:16:29 +0300

    Initial Commit

---
+++ RxDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RxDemo
+//
+//  Created by Nursultan on 20/11/2019.
+//  Copyright © 2019 Nursultan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
